814a828061bf35c5ddafc68502ed73ba0620cae1
Chad Danna <chad@formulated.io>
2016-09-11 16:55:53 -0600

    Create team-profile template > Refactor styling > Make sidebar additions

--- sidebar.php
+++ sidebar.php
@@ -50,9 +50,165 @@
     </ul>
   </section>
 
-  <div class="ad">
+  <section>
+    <section class="filters">
+      <h3>Team Stats</h3>
+      <div class="select-style">
+        <select name="filter">
+          <option selected="true" value="batting">Batting</option>
+        </select>
+      </div>
+      <a href="#">Full Stats</a>
+    </section>
+    <div class="data-table">
+      <div class="table-group">
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <td>vs. RPI/NPI Teams</td>
+                <td>RPI</td>
+                <td>NPI</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1-50</td>
+                <td>14-14</td>
+                <td>12-14</td>
+              </tr>
+              <tr>
+                <td>51-100</td>
+                <td>9-3</td>
+                <td>10-3</td>
+              </tr>
+              <tr>
+                <td>101-200</td>
+                <td>17-4</td>
+                <td>21-4</td>
+              </tr>
+              <tr>
+                <td>201+</td>
+                <td>5-0</td>
+                <td>2-0</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </section>
+
+  <section class="ad">
     <img src="img/ad-300x250.png" alt="" data-rjs="2"/>
-  </div>
+  </section>
+
+  <section>
+    <div class="data-table">
+      <div class="table-group">
+        <h4 class="category">SEC WEST STANDINGS</h4>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <td>Team</td>
+                <td>CONF</td>
+                <td>OVR</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>MSU</td>
+                <td>21-9</td>
+                <td>44-18-1</td>
+              </tr>
+              <tr>
+                <td>Texas A&M</td>
+                <td>20-10</td>
+                <td>49-16</td>
+              </tr>
+              <tr>
+                <td>LSU</td>
+                <td>19-11</td>
+                <td>45-21</td>
+              </tr>
+              <tr>
+                <td>Ole Miss</td>
+                <td>18-12</td>
+                <td>43-19</td>
+              </tr>
+              <tr>
+                <td>Ole Miss</td>
+                <td>18-12</td>
+                <td>43-19</td>
+              </tr>
+              <tr>
+                <td>Ole Miss</td>
+                <td>18-12</td>
+                <td>43-19</td>
+              </tr>
+              <tr>
+                <td>Ole Miss</td>
+                <td>18-12</td>
+                <td>43-19</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+      <div class="table-group">
+        <h4 class="category">SEC EAST STANDINGS</h4>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <td>Team</td>
+                <td>CONF</td>
+                <td>OVR</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>MSU</td>
+                <td>21-9</td>
+                <td>44-18-1</td>
+              </tr>
+              <tr>
+                <td>Texas A&M</td>
+                <td>20-10</td>
+                <td>49-16</td>
+              </tr>
+              <tr>
+                <td>LSU</td>
+                <td>19-11</td>
+                <td>45-21</td>
+              </tr>
+              <tr>
+                <td>Ole Miss</td>
+                <td>18-12</td>
+                <td>43-19</td>
+              </tr>
+              <tr>
+                <td>Ole Miss</td>
+                <td>18-12</td>
+                <td>43-19</td>
+              </tr>
+              <tr>
+                <td>Ole Miss</td>
+                <td>18-12</td>
+                <td>43-19</td>
+              </tr>
+              <tr>
+                <td>Ole Miss</td>
+                <td>18-12</td>
+                <td>43-19</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </section>
 
   <section id="twitter-feed">
     <div class="label red">
---
+++ team-profile.php
@@ -0,0 +1,457 @@
+<?php require 'header.php'; ?>
+
+<?php
+  $team_color = "rgba(70, 29, 124, 1.0)";
+  $team_logo = "img/team-logos/lsu-logo.png";
+?>
+<div id="wrap" class="container team-profile">
+  <div class="team-header" style="background: <?php echo $team_color; ?>;">
+    <img src="<?php echo $team_logo; ?>" alt="" data-rjs="2">
+    <div class="clearfix">
+      <div class="team-header-left">
+        <h1 class="team-title">LSU</h1>
+        <h3>45-21 (19-11 SEC)</h3>
+        <nav>
+          <a href="#">Schedule</a>
+          <a href="#">Statistics</a>
+          <a href="#">Standings</a>
+          <a href="#">Rankings</a>
+          <a href="#">By The Numbers</a>
+        </nav>
+      </div>
+      <div class="team-header-right pull-right">
+        <div class="score-board pull-left">
+          <div class="rpi"><span>RPI</span>9</div>
+          <div class="sos"><span>SOS</span>6</div>
+          <div class="t25"><span>T25</span>7</div>
+        </div>
+
+        <div class="team-logo pull-right">
+          <img src="<?php echo $team_logo; ?>" alt="" data-rjs="2">
+        </div>
+
+        <div class="clearfix"></div>
+        <div class="cite">
+          <span>Powered by <span>WarrenNolan.com</span></span>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <?php require 'sidebar.php'; ?>
+
+  <main>
+    <div class="content">
+
+      <section class="data-table schedule">
+        <section class="filters">
+          <h3>Schedule</h3>
+          <div class="select-style">
+            <select name="filter">
+              <option selected="true" value="2016">2016</option>
+              <option value="2015">2015</option>
+              <option value="2014">2014</option>
+              <option value="2013">2013</option>
+            </select>
+          </div>
+        </section>
+        <div>
+          <table>
+            <thead>
+              <tr>
+                <td>Date</td>
+                <td>Loc</td>
+                <td>Opp RPI</td>
+                <td>Opponent</td>
+                <td>Record</td>
+                <td>Results</td>
+                <td>Notes</td>
+              </tr>
+            </thead>
+          </table>
+        </div>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td class="date">Fri. Feb 19</td>
+                <td>vs</td>
+                <td>57</td>
+                <td><img src="img/team-logos/bearcat-logo.png" alt="" data-rjs="2"/> Cincinnati</td>
+                <td>26-30-1</td>
+                <td class="result win">6-5</td>
+                <td>[+]</td>
+              </tr>
+              <tr>
+                <td class="date">Fri. Feb 20</td>
+                <td>vs</td>
+                <td>57</td>
+                <td><img src="img/team-logos/bearcat-logo.png" alt="" data-rjs="2"/> Cincinnati</td>
+                <td>26-30-1</td>
+                <td class="result win">6-5</td>
+                <td>[+]</td>
+              </tr>
+              <tr>
+                <td class="date">Fri. Feb 21</td>
+                <td>vs</td>
+                <td>57</td>
+                <td><img src="img/team-logos/bearcat-logo.png" alt="" data-rjs="2"/> Cincinnati</td>
+                <td>26-30-1</td>
+                <td class="result win">6-5</td>
+                <td>[+]</td>
+              </tr>
+              <tr>
+                <td class="date">Wed. Feb 24</td>
+                <td>at</td>
+                <td>64</td>
+                <td><img src="img/team-logos/lamar-logo.png" alt="" data-rjs="2"/> Lamar</td>
+                <td>35-19</td>
+                <td class="result lose">11-2</td>
+                <td>[+]</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="special-tournament">
+          <table>
+            <tbody>
+              <tr>
+                <td class="date">Sat. Mar 5</td>
+                <td>vs</td>
+                <td>225</td>
+                <td><img src="img/team-logos/ballstate-logo.png" alt="" data-rjs="2"/> Ball State</td>
+                <td>32-26</td>
+                <td class="result win">11-2</td>
+                <td>[+]</td>
+              </tr>
+              <tr>
+                <td class="date">Sun. Mar 6</td>
+                <td>vs</td>
+                <td>225</td>
+                <td><img src="img/team-logos/ballstate-logo.png" alt="" data-rjs="2"/> Ball State</td>
+                <td>32-26</td>
+                <td class="result win">11-2</td>
+                <td>[+]</td>
+              </tr>
+              <tr>
+                <td class="date">Tues. Mar. 8</td>
+                <td>vs</td>
+                <td>77</td>
+                <td><img src="img/team-logos/neworleans-logo.png" alt="" data-rjs="2"/> New Orleans</td>
+                <td>31-26</td>
+                <td class="result win">11-2</td>
+                <td>[+]</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div>
+          <table>
+            <tbody>
+              <tr>
+                <td class="date">Fri. Feb 19</td>
+                <td>vs</td>
+                <td>57</td>
+                <td><img src="img/team-logos/bearcat-logo.png" alt="" data-rjs="2"/> Cincinnati</td>
+                <td>26-30-1</td>
+                <td class="result win">6-5</td>
+                <td>[+]</td>
+              </tr>
+              <tr>
+                <td class="date">Fri. Feb 20</td>
+                <td>vs</td>
+                <td>57</td>
+                <td><img src="img/team-logos/bearcat-logo.png" alt="" data-rjs="2"/> Cincinnati</td>
+                <td>26-30-1</td>
+                <td class="result win">6-5</td>
+                <td>[+]</td>
+              </tr>
+              <tr>
+                <td class="date">Fri. Feb 21</td>
+                <td>vs</td>
+                <td>57</td>
+                <td><img src="img/team-logos/bearcat-logo.png" alt="" data-rjs="2"/> Cincinnati</td>
+                <td>26-30-1</td>
+                <td class="result win">6-5</td>
+                <td>[+]</td>
+              </tr>
+              <tr>
+                <td class="date">Wed. Feb 24</td>
+                <td>at</td>
+                <td>64</td>
+                <td><img src="img/team-logos/lamar-logo.png" alt="" data-rjs="2"/> Lamar</td>
+                <td>35-19</td>
+                <td class="result lose">11-2</td>
+                <td>[+]</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+
+      <section class="data-table">
+        <section class="filters">
+          <h3>By The Numbers</h3>
+        </section>
+
+        <div class="pull-left half">
+          <div class="table-group">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td>Record</td>
+                    <td></td>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Overall</td>
+                    <td>45-21</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Home</td>
+                    <td>28-12</td>
+                  </tr>
+                  <tr>
+                    <td>Away</td>
+                    <td>13-8</td>
+                  </tr>
+                  <tr>
+                    <td>Neutral</td>
+                    <td>4-1</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Conference</td>
+                    <td>19-11</td>
+                  </tr>
+                  <tr>
+                    <td>Non-Conf</td>
+                    <td>20-6</td>
+                  </tr>
+                  <tr>
+                    <td>Postseason</td>
+                    <td>6-4</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="table-group">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td>Predictions</td>
+                    <td></td>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Overall</td>
+                    <td>45-21</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Conference</td>
+                    <td>28-12</td>
+                  </tr>
+                  <tr>
+                    <td>RPI</td>
+                    <td>9</td>
+                  </tr>
+                  <tr>
+                    <td>SOS</td>
+                    <td>6</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="table-group">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td>Runs Per Game</td>
+                    <td></td>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Offense</td>
+                    <td>6.5</td>
+                  </tr>
+                  <tr>
+                    <td>Defense</td>
+                    <td>4.5</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Margin</td>
+                    <td>+2.0</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+        <div class="pull-left half">
+          <div class="table-group">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td>Rankings</td>
+                    <td>Rank</td>
+                    <td>Value</td>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>RPI</td>
+                    <td>9</td>
+                    <td>0.5950</td>
+                  </tr>
+                  <tr>
+                    <td>Non-Conf RPI</td>
+                    <td>17</td>
+                    <td>0.5950</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>SOS</td>
+                    <td>6</td>
+                    <td>0.5950</td>
+                  </tr>
+                  <tr>
+                    <td>Non-Conf SOS</td>
+                    <td>32</td>
+                    <td>0.5462</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>NPI</td>
+                    <td>32</td>
+                    <td>78.580</td>
+                  </tr>
+                  <tr>
+                    <td>Elo-Chess</td>
+                    <td>8</td>
+                    <td>1745.0</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div class="table-group">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td>vs. RPI/NPI Teams</td>
+                    <td>RPI</td>
+                    <td>NPI</td>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>1-50</td>
+                    <td>14-14</td>
+                    <td>12-14</td>
+                  </tr>
+                  <tr>
+                    <td>51-100</td>
+                    <td>9-3</td>
+                    <td>10-3</td>
+                  </tr>
+                  <tr>
+                    <td>101-200</td>
+                    <td>17-4</td>
+                    <td>21-4</td>
+                  </tr>
+                  <tr>
+                    <td>201+</td>
+                    <td>5-0</td>
+                    <td>2-0</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+
+          <div class="table-group">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td>Current Streaks</td>
+                    <td></td>
+                  </tr>
+                </thead>
+              </table>
+            </div>
+            <div>
+              <table>
+                <tbody>
+                  <tr>
+                    <td>Overall Streak</td>
+                    <td>2 Losses</td>
+                  </tr>
+                  <tr>
+                    <td>Home Streak</td>
+                    <td>2 Losses</td>
+                  </tr>
+                  <tr>
+                    <td>Road Streak</td>
+                    <td>1 Loss</td>
+                  </tr>
+                </tbody>
+                <tbody>
+                  <tr>
+                    <td>Last 10</td>
+                    <td>6-4</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
+
+  <div class="clearfix"></div>
+</div>
+
+
+<?php require 'footer.php'; ?>
