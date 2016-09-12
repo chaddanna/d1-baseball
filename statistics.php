dae76d60360ee33419ea7280ac875d0378793c96
Chad Danna <chad@formulated.io>
2016-09-12 02:07:44 -0600

    Create statistics template

---
+++ statistics.php
@@ -0,0 +1,487 @@
+<?php require 'header.php'; ?>
+
+<div id="wrap" class="container statistics">
+
+  <main>
+    <div class="content">
+      <h1>Statistics</h1>
+
+      <section class="filters">
+        <form>
+          <div class="select-style">
+            <select name="year">
+              <option value="2016">2016</option>
+            </select>
+          </div>
+
+          <div class="select-style">
+            <select name="conference" required="">
+              <option>Conference</option>
+            </select>
+          </div>
+        </form>
+      </section>
+
+      <section class="data-table">
+        <h3>National Hitting Leaders</h3>
+        <div class="clearfix">
+          <div class="third pull-left">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td></td>
+                    <td>Player</td>
+                    <td>Batting Average</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                  <tr>
+                    <td>2</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                  <tr>
+                    <td>3</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="third pull-left">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td></td>
+                    <td>Player</td>
+                    <td>Homeruns</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+          <div class="third pull-left">
+            <div>
+              <table>
+                <thead>
+                  <tr>
+                    <td></td>
+                    <td>Player</td>
+                    <td>RBI</td>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>1</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                  <tr>
+                    <td>1</td>
+                    <td><a href="#">Ryan Scott, UALR</a></td>
+                    <td>.435</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
+        </div>
+
+
+          <h3>National Hitting Leaders</h3>
+          <div class="clearfix">
+            <div class="third pull-left">
+              <div>
+                <table>
+                  <thead>
+                    <tr>
+                      <td></td>
+                      <td>Player</td>
+                      <td>ERA</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div class="third pull-left">
+              <div>
+                <table>
+                  <thead>
+                    <tr>
+                      <td></td>
+                      <td>Player</td>
+                      <td>Strikeouts</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+            <div class="third pull-left">
+              <div>
+                <table>
+                  <thead>
+                    <tr>
+                      <td></td>
+                      <td>Player</td>
+                      <td>Saves</td>
+                    </tr>
+                  </thead>
+                  <tbody>
+                    <tr>
+                      <td>1</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                    <tr>
+                      <td>2</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                    <tr>
+                      <td>3</td>
+                      <td><a href="#">Ryan Scott, UALR</a></td>
+                      <td>.435</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+      </section>
+
+      <section class="data-table full-size">
+        <h3>Batting</h3>
+        <div class="clearfix">
+          <table>
+            <thead>
+              <tr>
+                <td></td>
+                <td>Player</td>
+                <td>Team</td>
+                <td>POS</td>
+                <td>AVG</td>
+                <td>OBP</td>
+                <td>SLG</td>
+                <td>OPS</td>
+                <td>GP</td>
+                <td>AB</td>
+                <td>R</td>
+                <td>H</td>
+                <td>2B</td>
+                <td>3b</td>
+                <td>HR</td>
+                <td>RBI</td>
+                <td>HP</td>
+                <td>BB</td>
+                <td>SO</td>
+                <td>SB</td>
+                <td>CS</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td><a href="#">Ryan Scott</a></td>
+                <td>Arkansas-Little Rock</td>
+                <td>OF</td>
+                <td>.435</td>
+                <td>.516</td>
+                <td>.713</td>
+                <td>1.229</td>
+                <td>54</td>
+                <td>216</td>
+                <td>56</td>
+                <td>94</td>
+                <td>20</td>
+                <td>2</td>
+                <td>12</td>
+                <td>66</td>
+                <td>10</td>
+                <td>27</td>
+                <td>33</td>
+                <td>7</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td><a href="#">Ryan Scott</a></td>
+                <td>Arkansas-Little Rock</td>
+                <td>OF</td>
+                <td>.435</td>
+                <td>.516</td>
+                <td>.713</td>
+                <td>1.229</td>
+                <td>54</td>
+                <td>216</td>
+                <td>56</td>
+                <td>94</td>
+                <td>20</td>
+                <td>2</td>
+                <td>12</td>
+                <td>66</td>
+                <td>10</td>
+                <td>27</td>
+                <td>33</td>
+                <td>7</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td><a href="#">Ryan Scott</a></td>
+                <td>Arkansas-Little Rock</td>
+                <td>OF</td>
+                <td>.435</td>
+                <td>.516</td>
+                <td>.713</td>
+                <td>1.229</td>
+                <td>54</td>
+                <td>216</td>
+                <td>56</td>
+                <td>94</td>
+                <td>20</td>
+                <td>2</td>
+                <td>12</td>
+                <td>66</td>
+                <td>10</td>
+                <td>27</td>
+                <td>33</td>
+                <td>7</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td><a href="#">Ryan Scott</a></td>
+                <td>Arkansas-Little Rock</td>
+                <td>OF</td>
+                <td>.435</td>
+                <td>.516</td>
+                <td>.713</td>
+                <td>1.229</td>
+                <td>54</td>
+                <td>216</td>
+                <td>56</td>
+                <td>94</td>
+                <td>20</td>
+                <td>2</td>
+                <td>12</td>
+                <td>66</td>
+                <td>10</td>
+                <td>27</td>
+                <td>33</td>
+                <td>7</td>
+                <td>5</td>
+              </tr>
+            </tbody>
+          </table>
+          <nav>
+            View
+            <a href="#">10</a>
+            <a href="#">100</a>
+            <a href="#">1000</a>
+            <a href="#">All</a>
+          </nav>
+        </div>
+
+        <h3>Pitching</h3>
+        <div class="clearfix">
+          <table>
+            <thead>
+              <tr>
+                <td></td>
+                <td>Player</td>
+                <td>Team</td>
+                <td>POS</td>
+                <td>AVG</td>
+                <td>OBP</td>
+                <td>SLG</td>
+                <td>OPS</td>
+                <td>GP</td>
+                <td>AB</td>
+                <td>R</td>
+                <td>H</td>
+                <td>2B</td>
+                <td>3b</td>
+                <td>HR</td>
+                <td>RBI</td>
+                <td>HP</td>
+                <td>BB</td>
+                <td>SO</td>
+                <td>SB</td>
+                <td>CS</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td><a href="#">Ryan Scott</a></td>
+                <td>Arkansas-Little Rock</td>
+                <td>OF</td>
+                <td>.435</td>
+                <td>.516</td>
+                <td>.713</td>
+                <td>1.229</td>
+                <td>54</td>
+                <td>216</td>
+                <td>56</td>
+                <td>94</td>
+                <td>20</td>
+                <td>2</td>
+                <td>12</td>
+                <td>66</td>
+                <td>10</td>
+                <td>27</td>
+                <td>33</td>
+                <td>7</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td><a href="#">Ryan Scott</a></td>
+                <td>Arkansas-Little Rock</td>
+                <td>OF</td>
+                <td>.435</td>
+                <td>.516</td>
+                <td>.713</td>
+                <td>1.229</td>
+                <td>54</td>
+                <td>216</td>
+                <td>56</td>
+                <td>94</td>
+                <td>20</td>
+                <td>2</td>
+                <td>12</td>
+                <td>66</td>
+                <td>10</td>
+                <td>27</td>
+                <td>33</td>
+                <td>7</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td><a href="#">Ryan Scott</a></td>
+                <td>Arkansas-Little Rock</td>
+                <td>OF</td>
+                <td>.435</td>
+                <td>.516</td>
+                <td>.713</td>
+                <td>1.229</td>
+                <td>54</td>
+                <td>216</td>
+                <td>56</td>
+                <td>94</td>
+                <td>20</td>
+                <td>2</td>
+                <td>12</td>
+                <td>66</td>
+                <td>10</td>
+                <td>27</td>
+                <td>33</td>
+                <td>7</td>
+                <td>5</td>
+              </tr>
+              <tr>
+                <td>1</td>
+                <td><a href="#">Ryan Scott</a></td>
+                <td>Arkansas-Little Rock</td>
+                <td>OF</td>
+                <td>.435</td>
+                <td>.516</td>
+                <td>.713</td>
+                <td>1.229</td>
+                <td>54</td>
+                <td>216</td>
+                <td>56</td>
+                <td>94</td>
+                <td>20</td>
+                <td>2</td>
+                <td>12</td>
+                <td>66</td>
+                <td>10</td>
+                <td>27</td>
+                <td>33</td>
+                <td>7</td>
+                <td>5</td>
+              </tr>
+            </tbody>
+          </table>
+          <nav>
+            View
+            <a href="#">10</a>
+            <a href="#">100</a>
+            <a href="#">1000</a>
+            <a href="#">All</a>
+          </nav>
+        </div>
+      </section>
+    </div>
+  </main>
+
+  <div class="clearfix"></div>
+</div>
+
+<?php require 'footer.php'; ?>
