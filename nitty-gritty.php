4f6c3bfcb076ff0a9b84d0091aa0336967576427
Chad Danna <chad@formulated.io>
2016-09-14 14:33:12 -0600

    Create Nitty Gritty template

---
+++ nitty-gritty.php
@@ -0,0 +1,524 @@
+<?php require 'header.php'; ?>
+
+<div id="wrap" class="container nitty-gritty">
+  <main>
+    <div class="player-header">
+      <div class="team-logo pull-left">
+        <img src="img/warren-nolan.png" alt="" data-rjs="2">
+      </div>
+      <div class="team-header-left">
+        <h1 class="team-title">Nitty Gritty Report</h1>
+        <div class="cite">
+          <span>Powered by <span>WarrenNolan.com</span></span>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+
+    <div class="content">
+      <div class="filters complex-table">
+        <span class="automatic-bid">Automatic Bid</span>
+        <span class="conference-leader">Conference Leader</span>
+        <span class="ineligible-postseason">Ineligible for Postseason</span>
+        <span class="key">1 - NCAA Violation, 2 - APR Penalty, 3 - Self-Imposed Ban, 4 - D-1 Transition</span>
+      </div>
+
+      <section class="data-table full-size complex-table">
+        <div class="clearfix">
+          <table>
+            <thead>
+              <tr class="main-tablehead">
+                <td colspan="2"></td>
+                <td colspan="3" class="align-center borders">Overall</td>
+                <td colspan="3" class="align-center borders">Non-Conference</td>
+                <td colspan="3" class="align-center borders">Location</td>
+                <td colspan="4" class="align-center borders">Vs. RPI Teams</td>
+                <td colspan="2" class="align-center">Average RPI</td>
+              </tr>
+              <tr class="sub-tablehead">
+                <td>RPI</td>
+                <td>Team</td>
+                <td>Record</td>
+                <td>RPI</td>
+                <td>SOS</td>
+                <td>Record</td>
+                <td>RPI</td>
+                <td>SOS</td>
+                <td>Home</td>
+                <td>Road</td>
+                <td>Neutral</td>
+                <td>1-50</td>
+                <td>51-100</td>
+                <td>101-200</td>
+                <td>200+</td>
+                <td>Wins</td>
+                <td>Losses</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr class="automatic-bid">
+                <td>3</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr class="conference-leader">
+                <td>4</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr class="ineligible-postseason">
+                <td>5</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>8</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">North Carolina State</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>9</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>10</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+        <div class="clearfix">
+          <table>
+            <thead>
+              <tr class="main-tablehead">
+                <td colspan="2"></td>
+                <td colspan="3" class="align-center borders">Overall</td>
+                <td colspan="3" class="align-center borders">Non-Conference</td>
+                <td colspan="3" class="align-center borders">Location</td>
+                <td colspan="4" class="align-center borders">Vs. RPI Teams</td>
+                <td colspan="2" class="align-center">Average RPI</td>
+              </tr>
+              <tr class="sub-tablehead">
+                <td>RPI</td>
+                <td>Team</td>
+                <td>Record</td>
+                <td>RPI</td>
+                <td>SOS</td>
+                <td>Record</td>
+                <td>RPI</td>
+                <td>SOS</td>
+                <td>Home</td>
+                <td>Road</td>
+                <td>Neutral</td>
+                <td>1-50</td>
+                <td>51-100</td>
+                <td>101-200</td>
+                <td>200+</td>
+                <td>Wins</td>
+                <td>Losses</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>1</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>2</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr class="automatic-bid">
+                <td>3</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr class="conference-leader">
+                <td>4</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr class="ineligible-postseason">
+                <td>5</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>6</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>7</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>8</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>9</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+              <tr>
+                <td>10</td>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>52-16</td>
+                <td>1</td>
+                <td>3</td>
+                <td>25-1</td>
+                <td>1</td>
+                <td>6</td>
+                <td>34-5</td>
+                <td>13-7</td>
+                <td>5-4</td>
+                <td>22-13</td>
+                <td>8-3</td>
+                <td>16-0</td>
+                <td>6-0</td>
+                <td>89</td>
+                <td>24</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </div>
+  </main>
+
+  <div class="clearfix"></div>
+</div>
+
+<?php require 'footer.php'; ?>
