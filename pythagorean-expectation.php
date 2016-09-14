3e60db7bed2ebdec3c428907b3cbd79c010409e8
Chad Danna <chad@formulated.io>
2016-09-14 15:37:45 -0600

    Create Pythagorean Expectation template

---
+++ pythagorean-expectation.php
@@ -0,0 +1,191 @@
+<?php require 'header.php'; ?>
+
+<div id="wrap" class="container pythagorean-expectation">
+  <main>
+    <div class="player-header">
+      <div class="team-logo pull-left">
+        <img src="img/warren-nolan.png" alt="" data-rjs="2">
+      </div>
+      <div class="team-header-left">
+        <h1 class="team-title">Pythagorean Expectation</h1>
+        <p>A team's runs scored and runs allowed to estimate how many games the team should have won and lost to date.</p>
+        <div class="cite">
+          <span>Powered by <span>WarrenNolan.com</span></span>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+
+    <div class="content">
+      <div class="filters complex-table">
+        <div class="good-luck">Good Luck is shown when a team's actual wins exceed expected wins by two or more.</div>
+        <div class="bad-luck">Bad Luck is shown when a team's actual wins are two or less than its expected wins.</div>
+      </div>
+
+      <section class="data-table complex-table">
+        <div class="clearfix">
+          <table>
+            <thead>
+              <tr class="main-tablehead">
+                <td></td>
+                <td colspan="2" class="align-center borders">Runs</td>
+                <td colspan="1" class="align-center borders">Pythagorean</td>
+                <td colspan="3" class="align-center borders">Overall</td>
+                <td colspan="3" class="align-center borders">Conference</td>
+                <td colspan="2" class="align-center">Predicted</td>
+              </tr>
+              <tr class="sub-tablehead">
+                <td>Team</td>
+                <td>Scored</td>
+                <td>Allowed</td>
+                <td>Expectation</td>
+                <td>Record</td>
+                <td>Pythag.</td>
+                <td>Luck</td>
+                <td>Record</td>
+                <td>Pythag.</td>
+                <td>Luck</td>
+                <td>Overall</td>
+                <td>Conf</td>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>471</td>
+                <td>202</td>
+                <td>0.8248</td>
+                <td>50-14</td>
+                <td>53-11</td>
+                <td><div class="bad-luck">-3</div></td>
+                <td>22-8</td>
+                <td>25-5</td>
+                <td><div class="bad-luck">-3</div></td>
+                <td>53-11</td>
+                <td>25-5</td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>471</td>
+                <td>202</td>
+                <td>0.8248</td>
+                <td>50-14</td>
+                <td>53-11</td>
+                <td></td>
+                <td>22-8</td>
+                <td>25-5</td>
+                <td></td>
+                <td>53-11</td>
+                <td>25-5</td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>471</td>
+                <td>202</td>
+                <td>0.8248</td>
+                <td>50-14</td>
+                <td>53-11</td>
+                <td></td>
+                <td>22-8</td>
+                <td>25-5</td>
+                <td></td>
+                <td>53-11</td>
+                <td>25-5</td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>471</td>
+                <td>202</td>
+                <td>0.8248</td>
+                <td>50-14</td>
+                <td>53-11</td>
+                <td></td>
+                <td>22-8</td>
+                <td>25-5</td>
+                <td><div class="bad-luck">-3</div></td>
+                <td>53-11</td>
+                <td>25-5</td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>471</td>
+                <td>202</td>
+                <td>0.8248</td>
+                <td>50-14</td>
+                <td>53-11</td>
+                <td><div class="bad-luck">-3</div></td>
+                <td>22-8</td>
+                <td>25-5</td>
+                <td><div class="good-luck">+6</div></td>
+                <td>53-11</td>
+                <td>25-5</td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>471</td>
+                <td>202</td>
+                <td>0.8248</td>
+                <td>50-14</td>
+                <td>53-11</td>
+                <td></td>
+                <td>22-8</td>
+                <td>25-5</td>
+                <td></td>
+                <td>53-11</td>
+                <td>25-5</td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>471</td>
+                <td>202</td>
+                <td>0.8248</td>
+                <td>50-14</td>
+                <td>53-11</td>
+                <td><div class="good-luck">+2</div></td>
+                <td>22-8</td>
+                <td>25-5</td>
+                <td><div class="good-luck">+6</div></td>
+                <td>53-11</td>
+                <td>25-5</td>
+              </tr>
+              <tr>
+                <td>
+                  <img class="team-logo" src="img/team-logos/Kendall%20Rogers%20-%20FloridaSEC.png" alt=""><a href="#">Florida</a>
+                </td>
+                <td>471</td>
+                <td>202</td>
+                <td>0.8248</td>
+                <td>50-14</td>
+                <td>53-11</td>
+                <td><div class="bad-luck">-7</div></td>
+                <td>22-8</td>
+                <td>25-5</td>
+                <td></td>
+                <td>53-11</td>
+                <td>25-5</td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </section>
+    </div>
+  </main>
+
+  <div class="clearfix"></div>
+</div>
+
+<?php require 'footer.php'; ?>
