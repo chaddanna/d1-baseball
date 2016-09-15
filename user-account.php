9ba1ee670a0a67dee7178c1f9b5ff6d97a64a83d
Chad Danna <chad@formulated.io>
2016-09-15 11:34:04 -0600

    Create User Account template

---
+++ user-account.php
@@ -0,0 +1,84 @@
+<?php require 'header.php'; ?>
+
+<div id="wrap" class="container user-account">
+  <main>
+    <div class="player-header">
+      <div class="team-logo pull-left">
+        <figure>
+          <img src="img/cameron.png" alt="" data-rjs="2">
+          <a href="#" class="edit">Edit</a>
+        </figure>
+      </div>
+      <div class="team-header-left">
+        <h1 class="team-title">My Account</h1>
+        <div>
+          <span class="user-name">Cameron Roberson</span>
+          <span class="sign-in">Not you? <a href="#">Sign Out</a>.</span>
+          <span class="email">cam427r@gmail.com</span>
+        </div>
+      </div>
+      <div class="clearfix"></div>
+    </div>
+
+    <div class="content">
+      <section>
+        <div class="clearfix">
+          <div class="pull-right subscription">
+            <h3>Subscription & Payment Info</h3>
+            <img src="img/premium-subscription.png" alt="" data-rjs="2"/>
+            <h4 class="category">Premium Monthly Subscription</h4>
+            <h4 class="related">Billed $12.99 Monthly to VISA 4884</h4>
+            <a href="#" class="button">Update Subscription</a>
+            <a href="#" class="switch">Save $57 a year ($4.75 monthly) by switching to an annual subscription.<br><br>
+            Switch Now!</a>
+          </div>
+          <div class="account-info">
+            <h3>Account Info</h3>
+            <a href="#" class="button">Manage Downloads</a>
+            <a href="#" class="button">Manage Orders</a>
+            <table>
+              <tr>
+                <td>Account</td>
+                <td>Cameron Roberson</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Username</td>
+                <td>cam427r</td>
+                <td></td>
+              </tr>
+              <tr>
+                <td>Password</td>
+                <td>********</td>
+                <td><a href="#" class="button">Update Password</a></td>
+              </tr>
+              <tr>
+                <td>Email</td>
+                <td>cameron@departmentof.design</td>
+                <td><a href="#" class="button">Update Email</a></td>
+              </tr>
+              <tr>
+                <td>Billing Address</td>
+                <td>
+                  <p>18016 Glen Park Dr.<br>Baton Rouge, Louisiana<br>70817</p>
+                </td>
+                <td><a href="#" class="button">Update Address</a></td>
+              </tr>
+              <tr>
+                <td>Payment Method</td>
+                <td>
+                  <p>Cameron M Roberson<br>VISA **** **** **** 4884</p>
+                </td>
+                <td><a href="#" class="button">Update Payment</a></td>
+              </tr>
+            </table>
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
+
+  <div class="clearfix"></div>
+</div>
+
+<?php require 'footer.php'; ?>
